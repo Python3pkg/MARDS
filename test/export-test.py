bac5baa6eb5d446b98fa12b9643aa27a5b8556e1
brad <bradc@makerredux.com>
2014-09-10 15:12:18 -0500

    a more complete example.  Fixed schema indention for workstep grammar.

---
+++ test/export-test.py
@@ -0,0 +1,23 @@
+import sys, os
+# the action below is a no-no in standard python, but it is done here for
+# easy on-the-fly testing.
+
+# this setup assumes that MARDS is NOT in the python library on the local
+# machine, but is instead in a parrallel directory.
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+#print sys.path
+import MARDS
+
+
+
+with open("export.MARDS", 'r') as docfile:
+    doc = docfile.read()
+with open("mr recipe.MARDS-schema", 'r') as schemafile:
+    schema = schemafile.read()
+
+#x,e = MARDS.ml._SCHEMA_to_rolne(schema)
+x,e = MARDS.string_to_rolne(doc, schema)
+print "FINAL:\n"
+print x._explicit()
+print "ERRORS:\n"
+print repr(e)
