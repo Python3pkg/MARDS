819ead50c7acfb3c57abda5e3e3d911acaf4e22e
brad <bradc@makerredux.com>
2014-09-16 14:19:46 -0500

    test compile file now takes command line arguments for input file, schema, and debug mode to compile the schema

--- test/export-test.py
+++ test/export-test.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, getopt
 # the action below is a no-no in standard python, but it is done here for
 # easy on-the-fly testing.
 
@@ -8,16 +8,45 @@ sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 #print sys.path
 import MARDS
 
+def main(argv):
+   inputfile = ''
+   sfile = 'mr recipe.MARDS-schema'
+   debug = False
+   try:
+      opts, args = getopt.getopt(argv,"hdi:o:",["ifile=","sfile=","debug"])
+   except getopt.GetoptError:
+      print 'test.py -i <docfile> -s <schemafile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'export-test.py -i <ifile> -s <sfile> (default = mr recipe.MARDS-schema> -d <debug>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-s", "--sfile"):
+         sfile = arg
+      elif opt in ("-d", "--debug"):
+         debug = True
+
+   print 'docfile is: ', inputfile
+   print 'Schema file is: ', sfile
+   with open(inputfile, 'r') as docfile:
+      doc = docfile.read()
+   with open(sfile, 'r') as schemafile:
+      schema = schemafile.read()
+
+   if debug == True:
+      x,e = MARDS.ml.SCHEMA_to_rolne(schema)
+   else:
+      x,e = MARDS.string_to_rolne(doc, schema)
+   
+   print "FINAL:\n"
+   print x._explicit()
+   print "ERRORS:\n"
+   print repr(e)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
 
-with open("export.MARDS", 'r') as docfile:
-    doc = docfile.read()
-with open("mr recipe.MARDS-schema", 'r') as schemafile:
-    schema = schemafile.read()
 
-#x,e = MARDS.ml.SCHEMA_to_rolne(schema)
-x,e = MARDS.string_to_rolne(doc, schema)
-print "FINAL:\n"
-print x._explicit()
-print "ERRORS:\n"
-print repr(e)
