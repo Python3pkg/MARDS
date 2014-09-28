369fc07389b651dd073a1c9919670ca22ba5b492
John Dupuy (from laptop) <john@cattailcreek9.com>
2014-09-28 01:19:26 -0500

    This branch will introduce autogeneration of documentation.
    Right now, all I've done is added a empty routine.

---
+++ MARDS/doc.py
@@ -0,0 +1,16 @@
+# -*- coding: iso-8859-1 -*-
+# MARDS\doc.py
+#
+# DOCUMENTATION GENERATION
+#
+
+from rolne import rolne
+
+# the breakdown_rolne contains instructions on how to "break down" the files used in the final docs.
+#
+def generate_rst_files(schema_rolne, breakdown_rolne, dest_dir, language="en"):
+    # create all_sub_docs
+    # create index.rst
+    return
+    
+# eof: MARDS\doc.py
