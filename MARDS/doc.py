99f7f0f9cd95c363baae6adf019371fdbed5b187
John Dupuy (from laptop) <john@cattailcreek9.com>
2014-09-28 15:36:34 -0500

    creates the files and title/abstract/body lines.
    Much left to do:
    
    * interpret 'value' section
    * go recursive; handle search/choice functions
    * writing main page
    * don't zap clean the directory; just the *.RST files
    * import bulk text where requested.

--- MARDS/doc.py
+++ MARDS/doc.py
@@ -9,8 +9,57 @@ from rolne import rolne
 # the breakdown_rolne contains instructions on how to "break down" the files used in the final docs.
 #
 def generate_rst_files(schema_rolne, breakdown_rolne, dest_dir, language="en"):
-    # create all_sub_docs
-    # create index.rst
+    docs = {}
+    # initialize with root doc
+    root = breakdown_rolne["root"]
+    docs[root.value] = []
+    # make subs
+    for sub in root.only("sub"):
+        if sub.get_value("new_doc")=="true":
+            docs[sub.value] = []
+            current = docs[sub.value]
+        else:
+            current = doc[root.value]
+        schema_sub = schema_rolne['name', sub.value]
+        # body
+        title = sub.value
+        if schema_sub.has(('describe', language)):
+            d = schema_sub['describe', language]
+            if d.has('title'):
+                title = d.get_value('title')
+            current.append(title)
+            current.append('====')
+            current.append('')
+            if d.has('abstract'):
+                current.append("''''")
+                current.append("Abstract")
+                current.append("''''")
+                current.append('')
+                current.append(d.get_value('abstract'))
+                current.append('')
+            if d.has('body'):
+                current.append("''''")
+                current.append("Body")
+                current.append("''''")
+                current.append('')
+                current.append(d.get_value('body'))
+                current.append('')
+        else:
+            current.append(title)
+            current.append('====')
+            current.append('')
+        # items list
+        for item in schema_sub.only('name'):
+            label = str(item.value)
+            if label[0:2]!="__":
+                current.append(label)
+    # generate everything
+    for key in docs:
+        fh = open(dest_dir+"/"+key+".rst", 'w')
+        for line in docs[key]:
+            fh.write(line)
+            fh.write('\n')
+        fh.close()
     return
     
 # eof: MARDS\doc.py
